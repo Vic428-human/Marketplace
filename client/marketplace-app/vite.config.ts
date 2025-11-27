25ff6450022d9863bcfd69a157ac18f80780e818
Vic428-human <z0983195379a@gmail.com>
2025-11-27 12:43:42 +0800

    set ngrok publish config.

--- client/marketplace-app/vite.config.ts
+++ client/marketplace-app/vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { ngrok } from "vite-plugin-ngrok";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+  server: {
+    allowedHosts: ["risky-unutterably-gia.ngrok-free.dev"],
+  },
+});
